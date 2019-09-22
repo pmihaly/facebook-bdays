015ee23f557fb0d7c244df5c1124b1ff546ccae1
Papp Mihály <papp.misi@protonmail.com>
2019-09-22 17:28:03 +0200

    ICS fájl letöltése

--- server.py
+++ server.py
@@ -1,11 +1,12 @@
-from flask import Flask, request
+from flask import Flask, request, Response
 from bdays import get_birthdays
 
 app = Flask(__name__)
 
 
 @app.route("/", methods=["POST"])
-def hello():
+def main_route():
     email = request.get_json()["email"]
     password = request.get_json()["pass"]
-    return get_birthdays(email, password)
+    return Response(get_birthdays(email, password), mimetype="text/ics", headers={"Content-disposition":
+                                                                                  "attachment; filename=birthdays.ics"})
