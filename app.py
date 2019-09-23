e2aac3874cb7890020d832112bd5b505b07d1573
Papp Mihály <papp.misi@protonmail.com>
2019-09-23 16:54:21 +0200

    Vue frontend létrehozása és kiszolgálása

--- app.py
+++ app.py
@@ -1,13 +1,22 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 from bdays import get_birthdays
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder="./dist/", template_folder="./dist")
 
 
 @app.route("/", methods=["POST"])
-def main_route():
+def bdays():
 
     return Response(
         get_birthdays(request.get_json()["email"], request.get_json()["pass"]),
         mimetype="text/ics",
         headers={"Content-disposition": "attachment; filename=birthdays.ics"})
+
+
+@app.route("/", methods=["GET"])
+def serve_frontend():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    app.run()
